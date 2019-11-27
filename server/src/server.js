ad5cff3f19ce438ef245b6395d66cfcaa7a3a5e4
JoBenjamin <jozsa.benjamin93@gmail.com>
2019-11-27 03:04:44 +0100

    Added basic mongoDB connection to backend server

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const mongourl = 'mongodb://localhost/test'; // Mongo DB URL later can be exported to ENV
+const mongoose = require('mongoose');
+
+mongoose.connect(mongourl, {useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {console.log('Successfully connected to MongoDB database.')})
+.catch((error) => {console.log('Database connection error: ' + error.message)});
 
 app.use(express.static('app'));
 
