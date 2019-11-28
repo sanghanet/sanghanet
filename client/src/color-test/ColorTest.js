050f080e1d5c00217cc2991cae7d588b46707a68
Misu Ballo <dizsmek@gmail.com>
2019-11-28 01:18:07 +0100

    finished website to be colored, and added font-awesome to it with React

--- client/src/color-test/ColorTest.js
+++ client/src/color-test/ColorTest.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import ProfilePic from './images/profile_pic.png'
 
 function ColorTest () {
     return (
@@ -6,8 +10,13 @@ function ColorTest () {
             <header>
                 <h1 className="page-title">SanghaNet</h1>
                 <div className="search-bar-grid">
-                    <div></div>
-                    <input type="text"/>
+                    <div>
+                        <FontAwesomeIcon
+                            className="search-icon"
+                            icon={faSearch}
+                        />
+                    </div>
+                    <input type="text" placeholder=" Search..."/>
                 </div>
                 <nav>
                     <a>HOME</a>
@@ -16,11 +25,60 @@ function ColorTest () {
                 </nav>
                 <div>
                     <label className="switch">
-                        <input type="checkbox" />
+                        <input type="checkbox"/>
                         <span className="slider round"></span>
                     </label>
                 </div>
             </header>
+
+            <main>
+                <div className="profile">
+                    <div className="profile-pic">
+                        <div>
+                            <img src={ProfilePic} />
+                        </div>
+                    </div>
+                    <div className="profile-pic-caption">
+                        <h3>John Doe</h3>
+                        <h3>Programmer</h3>
+                    </div>
+                </div>
+                <div className="personal-info">
+                    <div className="text-box">
+                        <h2>Personal information</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Error laboriosam id possimus harum molestiae delectus quis dolor, 
+                            dolorum provident amet? Nam esse nulla dolorem iste totam, error 
+                            harum ipsum eveniet!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Error laboriosam id possimus harum molestiae delectus quis dolor, 
+                            dolorum provident amet? Nam esse nulla dolorem iste totam, error 
+                            harum ipsum eveniet!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Error laboriosam id possimus harum molestiae delectus quis dolor, 
+                            dolorum provident amet? Nam esse nulla dolorem iste totam, error 
+                            harum ipsum eveniet!
+                        </p>
+                    </div>
+                    <div>
+                        <a className="edit-button">
+                        <FontAwesomeIcon
+                            className="edit-icon"
+                            icon={faEdit}
+                        />
+                            EDIT
+                        </a>
+                    </div>
+                </div>
+            </main>
+            <footer>
+                <h3>Hello World!</h3>
+            </footer>
         </div>
     )
 }
