c71f9f9b6f8c8de7acfaed934b49ff81766b3652
Misu Ballo <dizsmek@gmail.com>
2019-12-10 19:08:57 +0100

    conditional rendering works on page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,21 +3,37 @@ import Buddha from './media/sangha_logo.svg';
 import GoogleLogo from './media/google_logo.png';
 
 class Login extends Component {
+    constructor (props) {
+        super(props);
+        this.state = { login: false };
+    }
+
+    logIn = () => {
+        this.setState({ login: true });
+    }
+
     render () {
+        if (!this.state.login) {
+            return (
+                <div className="app">
+                    <header>
+                        <h1>Welcome to SanghaNet</h1>
+                    </header>
+                    <main>
+                        <img className="buddha" src={Buddha} alt="buddha logo"/>
+                        <button onClick={this.logIn} className="login-btn">
+                            <div className="google-logo-box">
+                                <img className="google-logo" src={GoogleLogo} alt="google logo"/>
+                            </div>
+                            <div className="login-btn-text noselect">SIGN IN</div>
+                        </button>
+                    </main>
+                </div>
+            );
+        }
         return (
-            <div className="app">
-                <header>
-                    <h1>Welcome to SanghaNet</h1>
-                </header>
-                <main>
-                    <img className="buddha" src={Buddha} alt="buddha logo"/>
-                    <button className="login-btn">
-                        <div className="google-logo-box">
-                            <img className="google-logo" src={GoogleLogo} alt="google logo"/>
-                        </div>
-                        <div className="login-btn-text noselect">SIGN IN</div>
-                    </button>
-                </main>
+            <div>
+                <h1>Hello World</h1>
             </div>
         );
     }
