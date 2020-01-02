116584bc4a17de2130a6fad4df83a0a0c9095f39
Ildiko Rigo <ildiko.bcb@gmail.com>
2020-01-02 20:39:31 +0100

    Header layout for tablet and mobile

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,17 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Navbar_Header.scss';
+import Avatar from './icons/avatar.png';
 import { ReactComponent as Search } from './icons/search.svg';
 
 const Header = (props) => {
     return (
         <header className='header'>
-            <p>Signed in as <br/><span> Jon Doe</span></p>
+            <p><img src={Avatar} alt="Profile image" className="avatar"/><br/><span> Jon Doe</span></p>
             <div className="search-field">
                 <input type="text" placeholder="Search..."></input>
                 <button><Search /></button>
             </div>
-            <p>{props.activePage}</p>
+            <p className="page-name">{props.activePage}</p>
+            <div className="mobile-header">
+                <button className="mobile-search">
+                    <Search className="mobile-search-icon" />
+                </button>
+                <button className="burger-lines">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
+            </div>
         </header>
     );
 };
