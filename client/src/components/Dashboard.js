7bfb97ff36ef99ae33d093281070d2b66633bb65
Misu Ballo <dizsmek@gmail.com>
2020-01-11 19:55:56 +0100

    Search component works for user names

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -10,40 +10,12 @@ class Dashboard extends React.Component {
         };
     }
 
-    fetchData = () => {
-        fetch('http://localhost:4000/userList', { method: 'GET' })
-            .then((res) => {
-                return res.json();
-            }).then((data) => {
-                this.printUserList(data);
-            }).catch((err) => {
-                throw new Error(err.message);
-            });
-    }
-
-    printUserList = (userArray) => {
-        const result = userArray.map((value, index) => {
-            return (
-                <div key={value._id}>
-                    {index} {value.first_name} {value.last_name}
-                </div>
-            );
-        });
-
-        this.setState({ list: result });
-    }
-
     render () {
         return (
             <main>
                 <h1>DASHBOARD, OF COURSE.</h1>
                 {/* <input type="text" name="searchUsers" className="user-search"/> */}
-                <SearchBar
-                    className="user-search"
-                    searchAction={() => console.log('test works') }
-                />
-                <button onClick = {this.fetchData}>List users</button>
-                <div>{this.state.list}</div>
+                <SearchBar className="user-search" buttonValue="Search" />
             </main>
         );
     }
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -9,9 +9,30 @@ class SearchBar extends Component {
         };
     }
 
+    fetchData = () => {
+        fetch('http://localhost:4000/userList', { method: 'GET' })
+            .then((res) => {
+                return res.json();
+            }).then((data) => {
+                this.setState({ dataList: data });
+                this.handleSearch(data);
+            }).catch((err) => {
+                throw new Error(err.message);
+            });
+    }
+
     handleInputChange = (e) => {
         this.setState({ inputValue: e.target.value });
-        console.dir(this.state.inputValue);
+    }
+
+    handleSearch = (users) => {
+        const userNames = users.map((user) => {
+            return `${user.first_name} ${user.last_name}`;
+        });
+
+        const foundUsers = userNames.filter(name => name.includes(this.state.inputValue));
+
+        console.dir(foundUsers);
     }
 
     render () {
@@ -22,15 +43,24 @@ class SearchBar extends Component {
                     name="searchUsers"
                     onChange={this.handleInputChange}
                     value={this.state.inputValue}
+                    className={this.props.inputClassName}
                 />
-                <button onClick={this.props.searchAction}>Search</button>
+                <button
+                    onClick={this.fetchData}
+                    className={this.props.buttonClassName}
+                >
+                    {this.props.buttonValue}
+                </button>
             </div>
         );
     }
 }
 
 SearchBar.propTypes = {
-    searchAction: PropTypes.func
+    // in case inner JSX elements need to be styled when element is reused
+    inputClassName: PropTypes.string,
+    buttonClassName: PropTypes.string,
+    buttonValue: PropTypes.string
 };
 
 export default SearchBar;
