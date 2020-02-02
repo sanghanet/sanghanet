28182543dc14a6f521e8e3ad254e12f0b399b4a8
Misu Ballo <dizsmek@gmail.com>
2020-02-02 14:10:12 +0100

    footer element added to Queries

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.scss';
+
+const Footer = (props) => {
+    return (
+        <footer className='footer'>
+            asdf
+        </footer>
+    );
+};
+
+export default Footer;
--- client/src/pages/Queries.js
+++ client/src/pages/Queries.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 const Queries = (props) => {
@@ -10,6 +11,7 @@ const Queries = (props) => {
             <Navbar />
             <main>
                 <h1>QUERIES ARE REPORTING FOR DUTY!</h1>
+                <Footer />
             </main>
         </div>
     );
