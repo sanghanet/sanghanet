e0061cd5bbd7f420514006c6ca0bd408b6fcac1d
JoBenjamin <jozsa.benjamin93@gmail.com>
2020-02-11 21:33:53 +0100

    Removed dashboard user list code from both backend and frontend

--- server/src/server.js
+++ server/src/server.js
@@ -79,11 +79,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/userList', (req, res) => {
-    res.set({ 'Access-Control-Allow-Origin': '*' });
-    coll.find({}).toArray().then((data) => { res.json(data); });
-});
-
 app.post('/auth',
     passport.authenticate(
         'google',
