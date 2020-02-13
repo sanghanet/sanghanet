1b7b7f0d91c59a9e827f3a7493da5403b7b094bb
Misu Ballo <dizsmek@gmail.com>
2020-02-13 15:01:17 +0100

    navbar added back

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 import './Profile.scss';
@@ -37,6 +38,7 @@ class Profile extends React.Component {
         return (
             <div>
                 <Header activePage="Profile" />
+                <Navbar />
                 <main>
                     <form className="profile-form">
                         <div className="general-data profile-blocks">
