1f3d366ddd27b84d873570453987996ef2e1d098
Misu Ballo <dizsmek@gmail.com>
2020-02-13 15:10:29 +0100

    from class component to function component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
@@ -9,72 +9,61 @@ import PageNavigation from './PageNavigation';
 import { ReactComponent as SearchIcon } from './icons/search.svg';
 import { Container, Row, Figure, Button } from 'react-bootstrap';
 
-class Header extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            navOpen: true
-        };
-    }
-
-    handleAvatarClick = (event) => {
+const Header = (props) => {
+    const handleAvatarClick = (event) => {
         if (this.props.location.pathname !== '/profile') {
             this.props.history.push('/profile');
         }
     };
 
-    handleHamburgerClick = () => {
-        this.setState((state) => ({ navOpen: !state.navOpen }));
-
+    const handleHamburgerClick = () => {
         const slider = document.getElementsByClassName('slider')[0];
         const hamburger = document.getElementsByClassName('burger-lines')[0];
 
         slider.classList.toggle('slideIn');
         hamburger.classList.toggle('activeBurger');
-    }
+    };
 
-    render () {
-        return (
-            <Container fluid className='header d-flex p-0' as='header'>
-                <Row className='d-flex'>
-                    <Figure
-                        bsPrefix='avatar-container d-md-flex d-grid m-0'
-                        onClick={this.handleAvatarClick}
-                    >
-                        <Figure.Image
-                            src={Avatar}
-                            alt='Profile'
-                            roundedCircle
-                            width={70}
-                            height={70}
-                        />
-                        <Figure.Caption bsPrefix='avatar-name d-none d-sm-flex' as='h2'>
-                            {sessionStorage.user}
-                        </Figure.Caption>
-                    </Figure>
-                    <SearchBar className='d-md-flex d-none'/>
-                    <h1 className='page-name m-0 d-none d-md-flex'>{this.props.activePage}</h1>
+    return (
+        <Container fluid className='header d-flex p-0' as='header'>
+            <Row className='d-flex'>
+                <Figure
+                    bsPrefix='avatar-container d-md-flex d-grid m-0'
+                    onClick={handleAvatarClick}
+                >
+                    <Figure.Image
+                        src={Avatar}
+                        alt='Profile'
+                        roundedCircle
+                        width={70}
+                        height={70}
+                    />
+                    <Figure.Caption bsPrefix='avatar-name d-none d-sm-flex' as='h2'>
+                        {sessionStorage.user}
+                    </Figure.Caption>
+                </Figure>
+                <SearchBar className='d-md-flex d-none'/>
+                <h1 className='page-name m-0 d-none d-md-flex'>{props.activePage}</h1>
 
-                    <Button className='search-icon d-flex d-md-none' variant='outline-light'>
-                        <SearchIcon/>
-                    </Button>
+                <Button className='search-icon d-flex d-md-none' variant='outline-light'>
+                    <SearchIcon/>
+                </Button>
 
-                    <button
-                        className='burger-lines p-0 d-flex d-md-none position-absolute'
-                        onClick={this.handleHamburgerClick}
-                    >
-                        <input type='checkbox' />
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </button>
-                    <div className='slider position-absolute'>
-                        <PageNavigation />
-                    </div>
-                </Row>
-            </Container>
-        );
-    }
+                <button
+                    className='burger-lines p-0 d-flex d-md-none position-absolute'
+                    onClick={handleHamburgerClick}
+                >
+                    <input type='checkbox' />
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
+                <div className='slider position-absolute'>
+                    <PageNavigation />
+                </div>
+            </Row>
+        </Container>
+    );
 };
 
 Header.propTypes = {
