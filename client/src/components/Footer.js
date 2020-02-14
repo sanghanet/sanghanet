102628e19341f09202407fb610f9ff1904a135fa
Misu Ballo <dizsmek@gmail.com>
2020-02-14 12:09:26 +0100

    footer is implemented

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Navbar_Header.scss';
+import './Footer.scss';
 
 const Footer = (props) => {
     return (
-        <footer className='footer'>
+        <footer className='footer position-absolute p-0 m-0'>
             <div className="credential-box">
                 <p>Made by:
                     <span>Ildiko Rigo</span>
