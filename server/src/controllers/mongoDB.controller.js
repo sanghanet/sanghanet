7da87508baebdbe60d9a1b990b8bf4c67c32be89
Spartan <spartan@enigma.com>
2020-02-25 01:07:38 +0100

    Remove code from try block

--- server/src/controllers/mongoDB.controller.js
+++ server/src/controllers/mongoDB.controller.js
@@ -5,31 +5,30 @@ const log4js = require('log4js');
 const log = log4js.getLogger('controllers/mongoDB.controller.js');
 
 const initDBConnection = async () => {
-    try {
-        mongoose.connection.on('connected', () => {
-            log.info('Successfully connected to: ', DB_URL);
-        });
+    mongoose.connection.on('connected', () => {
+        log.info('Successfully connected to: ', DB_URL);
+    });
 
-        mongoose.connection.on('disconnected', () => {
-            log.error('MongoDB disconnected!');
-        });
+    mongoose.connection.on('disconnected', () => {
+        log.error('MongoDB disconnected!');
+    });
 
-        mongoose.connection.on('reconnected', () => {
-            log.info('MongoDB connection reestablished');
-        });
+    mongoose.connection.on('reconnected', () => {
+        log.info('MongoDB connection reestablished');
+    });
 
-        mongoose.connection.on('error', (error) => {
-            log.fatal('MongoDB error: ', error.message);
+    mongoose.connection.on('error', (error) => {
+        log.fatal('MongoDB error: ', error.message);
+        log4js.shutdown(process.exit);
+    });
+
+    process.on('SIGINT', () => {
+        mongoose.connection.close(() => {
+            log.fatal('MongoDB connection is disconnected due to application termination');
             log4js.shutdown(process.exit);
         });
-
-        process.on('SIGINT', () => {
-            mongoose.connection.close(() => {
-                log.fatal('MongoDB connection is disconnected due to application termination');
-                log4js.shutdown(process.exit);
-            });
-        });
-
+    });
+    try {
         await mongoose.connect(
             DB_URL, {
                 useNewUrlParser: true,
