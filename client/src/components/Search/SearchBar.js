7f3361dd1f888bd9876a8e2247bc8872ad77e983
Misu Ballo <dizsmek@gmail.com>
2020-02-29 16:54:43 +0100

    Select tag styling

--- client/src/components/Search/SearchBar.js
+++ client/src/components/Search/SearchBar.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './SearchBar.scss';
-import { ReactComponent as Search } from '../icons/search.svg';
 
 class SearchBar extends Component {
     constructor (props) {
