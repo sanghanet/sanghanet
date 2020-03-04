0c14e1448d12826d8ecddb9b539f520cf95121fb
Misu Ballo <dizsmek@gmail.com>
2020-03-04 20:07:51 +0100

    commented out unnecessary lines

--- server/src/routers/router.js
+++ server/src/routers/router.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const log4js = require('log4js');
-const log = log4js.getLogger('routers/routes.js');
+// const log4js = require('log4js');
+// const log = log4js.getLogger('routers/routes.js');
 
 const userController = require('../controllers/user.controller');
 
 router.use((req, res, next) => {
-    // this can be uncommented once ticket #45 is solved
+    // this can be uncommented once ticket #45 is solved, as well as the log variable
     // log.info(`[${req.ip}] ${req.method} ${req.url}, ${req.user.email}`);
     next();
 });
