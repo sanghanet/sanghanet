64bebb18b33308834c8e27f1b301713f76a76021
Spartan <spartan@enigma.com>
2020-03-07 16:53:15 +0100

    Fix Issue#54, When session expired, logout generate error in BE, and 500 (Internal Server Error) on FE

--- server/src/routers/router.js
+++ server/src/routers/router.js
@@ -7,8 +7,12 @@ const log = log4js.getLogger('routers/router.js');
 const userController = require('../controllers/user.controller');
 
 router.use((req, res, next) => {
-    log.info(`[${req.ip}] ${req.method} ${req.url}, ${req.user.email}`);
-    next();
+    if (!req.user) { // session cookie has expired
+        res.redirect('/');
+    } else {
+        log.info(`[${req.ip}] ${req.method} ${req.url}, ${req.user.email}`);
+        next();
+    }
 });
 
 router.post('/user/login', userController.login);
