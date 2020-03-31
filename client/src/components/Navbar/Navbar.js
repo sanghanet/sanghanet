22cc2cbd0c0c171d248c4c83db2f8819c11bc7ed
Misu Ballo <dizsmek@gmail.com>
2020-03-31 12:33:32 +0200

    navbar is now a class component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
@@ -13,69 +13,81 @@ import { ReactComponent as YogaIcon } from '../icons/yoga.svg';
 import { ReactComponent as EventIcon } from '../icons/event.svg';
 import { ReactComponent as QuestionsIcon } from '../icons/questions.svg';
 import { ReactComponent as SuperuserIcon } from '../icons/superuser.svg';
+import { Component } from 'react';
 
-const Navbar = (props) => {
-    const { activePage, navStyle } = props;
+class Navbar extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            scrollPos: 0
+        }
+    }
+
+    activePage = props.activePage;
+    navStyle = props.navStyle;
     // navStyle has two CSS id: sidenav, hamburger
-    return (
-        <ul onClick={activePage} className="navigation" id={navStyle}>
-            <li>
-                <NavLink exact to="/dashboard" className="link">
-                    <div className="menu-icon"><DashboardIcon /></div>
-                    <span className="title">Dashboard</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/personal" className="link">
-                    <div className="menu-icon"><PersonalIcon /></div>
-                    <span className="title">Personal Data</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/yoga" className="link">
-                    <div className="menu-icon"><YogaIcon /></div>
-                    <span className="title">Yoga</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/finances" className="link">
-                    <div className="menu-icon"><FinanceIcon /></div>
-                    <span className="title">Finances</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/events" className="link">
-                    <div className="menu-icon"><EventIcon /></div>
-                    <span className="title">Events</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/questions" className="link">
-                    <div className="menu-icon"><QuestionsIcon /></div>
-                    <span className="title">Personal Questions</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/queries" className="link">
-                    <div className="menu-icon"><InfoIcon /></div>
-                    <span className="title">Queries</span>
-                </NavLink>
-            </li>
-            { sessionStorage.isSuperuser === 'true'
-                ? <li>
-                    <NavLink exact to="/superuser" className="link">
-                        <div className="menu-icon"><SuperuserIcon /></div>
-                        <span className="title">Superuser</span>
+
+    render() {
+        return (
+            <ul onClick={activePage} className="navigation" id={navStyle}>
+                <li>
+                    <NavLink exact to="/dashboard" className="link">
+                        <div className="menu-icon"><DashboardIcon /></div>
+                        <span className="title">Dashboard</span>
                     </NavLink>
                 </li>
-                : null
-            }
-            <li>
-                <Logout />
-            </li>
-        </ul>
-    );
-};
+                <li>
+                    <NavLink exact to="/personal" className="link">
+                        <div className="menu-icon"><PersonalIcon /></div>
+                        <span className="title">Personal Data</span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink exact to="/yoga" className="link">
+                        <div className="menu-icon"><YogaIcon /></div>
+                        <span className="title">Yoga</span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink exact to="/finances" className="link">
+                        <div className="menu-icon"><FinanceIcon /></div>
+                        <span className="title">Finances</span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink exact to="/events" className="link">
+                        <div className="menu-icon"><EventIcon /></div>
+                        <span className="title">Events</span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink exact to="/questions" className="link">
+                        <div className="menu-icon"><QuestionsIcon /></div>
+                        <span className="title">Personal Questions</span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink exact to="/queries" className="link">
+                        <div className="menu-icon"><InfoIcon /></div>
+                        <span className="title">Queries</span>
+                    </NavLink>
+                </li>
+                { sessionStorage.isSuperuser === 'true'
+                    ? <li>
+                        <NavLink exact to="/superuser" className="link">
+                            <div className="menu-icon"><SuperuserIcon /></div>
+                            <span className="title">Superuser</span>
+                        </NavLink>
+                    </li>
+                    : null
+                }
+                <li>
+                    <Logout />
+                </li>
+            </ul>
+        );
+    }
+}
 
 Navbar.propTypes = {
     activePage: PropTypes.func,
