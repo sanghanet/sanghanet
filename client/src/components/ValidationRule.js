8a6aaef64f2b238c4156d0f45bcf7be535f04643
Misu Ballo <dizsmek@gmail.com>
2020-05-31 01:05:19 +0200

    email address validation implemented

--- client/src/components/ValidationRule.js
+++ client/src/components/ValidationRule.js
@@ -7,6 +7,8 @@ const nameValidationRule = {
 };
 const addressPattern = '^[0-9]{4} [A-ZÁÉÚŐÓÜÖÍa-záéúőóüöí0-9.,/\\s-]*$';
 const mobilePattern = '[0-9]{2}/[0-9]{2}-[0-9]{2}-[0-9]{3}';
+// eslint-disable-next-line no-control-regex
+const emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@gmail.com/;
 
 const validationError = (input) => {
     if (input.validity.valid) {
@@ -34,5 +36,6 @@ export {
     nameValidationRule,
     addressPattern,
     mobilePattern,
-    validationError
+    validationError,
+    emailPattern
 };
--- client/src/pages/Superuser/AddMemberDialog/AddMemberDialog.js
+++ client/src/pages/Superuser/AddMemberDialog/AddMemberDialog.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import GenericDialog from '../../../components/Form/GenericDialog/GenericDialog';
 
 import './AddMemberDialog.scss';
+import { emailPattern } from '../../../components/ValidationRule';
 
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
@@ -17,8 +18,14 @@ class AddMemberDialog extends Component {
 
     customValidation = () => {
         const { emailInputValue, labelInputValue } = this.state;
-        const errorMsg = emailInputValue.includes('@') ? 'Leave \'@gmail.com\' off' : '';
-        const isInvalid = !(!errorMsg.length && emailInputValue.length && labelInputValue.length);
+
+        const errorMsg = emailInputValue.includes('@')
+            ? 'Leave \'@gmail.com\' off'
+            : emailPattern.test(`${emailInputValue}@gmail.com`)
+                ? ''
+                : 'Invalid email address';
+
+        const isInvalid = !(!errorMsg.length && labelInputValue.length);
         this.setState({
             isDisabled: isInvalid,
             errorMsg: errorMsg
@@ -59,16 +66,17 @@ class AddMemberDialog extends Component {
                         value={labelInputValue}
                         id="label-input"
                         onChange={this.handleLabelChange}
+                        autoFocus
                     >
                     </Form.Control>
                     <Form.Label htmlFor="email-input">Enter email address to add:</Form.Label>
                     <InputGroup>
                         <Form.Control
+                            className={errorMsg.length ? 'email-input invalid' : 'email-input'}
                             type="text"
                             value={emailInputValue}
                             id="email-input"
                             onChange={this.handleEmailChange}
-                            autoFocus
                         >
                         </Form.Control>
                         <InputGroup.Append>
