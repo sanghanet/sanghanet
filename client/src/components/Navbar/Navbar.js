d9561146dbe0fe17ebfe332a5bc2d75f35806221
Ildiko Rigo <ildiko.bcb@gmail.com>
2020-06-01 17:21:20 +0200

    Pretty admins dropdown

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -15,6 +15,10 @@ import { ReactComponent as QuestionsIcon } from '../icons/questions.svg';
 import { ReactComponent as SuperuserIcon } from '../icons/superuser.svg';
 
 class Navbar extends Component {
+    state = {
+        dropdownVisible: false
+    }
+
     componentDidMount () {
         const desktopMenu = document.getElementById('sidenav');
         const hamburgerMenu = document.getElementsByClassName('slider')[0];
@@ -51,7 +55,12 @@ class Navbar extends Component {
         }
     }
 
+    handleAdminsDropdown = () => {
+        this.setState((prevState) => ({ dropdownVisible: !prevState.dropdownVisible }));
+    }
+
     render () {
+        const { dropdownVisible } = this.state;
         return (
             <ul className="navigation" id={this.props.navStyle}>
                 <li>
@@ -101,6 +110,28 @@ class Navbar extends Component {
                         <div className="menu-icon"><SuperuserIcon /></div>
                         <span className="title">Superuser</span>
                     </NavLink>
+                </li>
+                <li>
+                    <div className="link" id="admins" onClick={this.handleAdminsDropdown}>
+                        <div className="menu-icon"><SuperuserIcon /></div>
+                        <span className="title">Admins</span>
+                        { dropdownVisible &&
+                            (<ul id="dropdown" onMouseLeave={() => { this.setState({ dropdownVisible: false }); }}>
+                                <li>
+                                    <NavLink exact to="/finances" className="dropdown-link">Finance</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink exact to="/events" className="dropdown-link">Events</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink exact to="/yoga" className="dropdown-link">Yoga</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink exact to="/superuser" className="dropdown-link">Superuser</NavLink>
+                                </li>
+                            </ul>)
+                        }
+                    </div>
                 </li>
                 <li id="logout-li">
                     <Logout />
