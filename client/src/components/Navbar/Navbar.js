a73bc77a8e20921da66c647baff89d8d876415cd
Misu Ballo <dizsmek@gmail.com>
2020-06-02 00:03:36 +0200

    rearranged admin pages button to top

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -20,9 +20,8 @@ class Navbar extends Component {
     }
 
     toggleAdminPages = () => {
-        document.getElementById('sidenav').childNodes[0].classList.toggle('slide-in');
-        document.getElementById('sidenav').childNodes[1].classList.toggle('slide-away');
-        console.dir(document.getElementById('sidenav').childNodes[1]);
+        // document.getElementById('sidenav').childNodes[0].classList.toggle('slide-in');
+        document.getElementById('sidenav').childNodes[1].classList.toggle('slide-out');
         this.setState((prevState) => ({ showAdminPages: !prevState.showAdminPages }));
     }
 
@@ -30,6 +29,7 @@ class Navbar extends Component {
         const desktopMenu = document.getElementById('sidenav');
         const hamburgerMenu = document.getElementsByClassName('slider')[0];
 
+        // TODO: readjust scroll positions
         switch (window.location.pathname) {
             case '/finances':
                 hamburgerMenu.scrollTop = 50;
@@ -52,8 +52,8 @@ class Navbar extends Component {
                 break;
 
             case '/superuser':
-                hamburgerMenu.scrollTop = 500;
-                desktopMenu.scrollTop = 300;
+                // hamburgerMenu.scrollTop = 500;
+                // desktopMenu.scrollTop = 300;
                 break;
 
             default:
@@ -63,10 +63,11 @@ class Navbar extends Component {
     }
 
     render () {
+        const { showAdminPages } = this.state;
         return (
-            <div className="navigation" id={this.props.navStyle}>
-                <ul className="admin-items">
-                    <li onClick={this.toggleAdminPages}>Back</li>
+            <div className={`navigation${showAdminPages ? ' overflow-hidden' : ''}`} id={this.props.navStyle}>
+                <ul className={`admin-items${showAdminPages ? ' slide-in' : ''}`}>
+                    <li className="link" onClick={this.toggleAdminPages}>Back</li>
                     <li>
                         <NavLink exact to="/superuser" className="link">
                             <div className="menu-icon"><DashboardIcon /></div>
@@ -74,7 +75,13 @@ class Navbar extends Component {
                         </NavLink>
                     </li>
                 </ul>
-                <ul className="general-items">
+                <ul className={`general-items${showAdminPages ? ' slide-out' : ''}`}>
+                    <li>
+                        <div className="link" onClick={this.toggleAdminPages}>
+                            <div className="menu-icon"><SuperuserIcon /></div>
+                            <span className="title">Admin pages</span>
+                        </div>
+                    </li>
                     <li>
                         <NavLink exact to="/dashboard" className="link">
                             <div className="menu-icon"><DashboardIcon /></div>
@@ -117,13 +124,6 @@ class Navbar extends Component {
                             <span className="title">Queries</span>
                         </NavLink>
                     </li>
-                    <li>
-                        <NavLink exact to="/superuser" className="link">
-                            <div className="menu-icon"><SuperuserIcon /></div>
-                            <span className="title">Superuser</span>
-                        </NavLink>
-                    </li>
-                    <li className="link admin-pages" onClick={this.toggleAdminPages}>Admin pages</li>
                     <li id="logout-li">
                         <Logout />
                     </li>
