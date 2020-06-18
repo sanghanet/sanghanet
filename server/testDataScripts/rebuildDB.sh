5ec5d5284ad5acee8c818e9100f52e494951fa4e
Misu Ballo <dizsmek@gmail.com>
2020-06-19 00:17:37 +0200

    help supplied when no arguments were given

--- server/testDataScripts/rebuildDB.sh
+++ server/testDataScripts/rebuildDB.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "For local action, write: $0 local"
+    echo "For atlas action, write: $0 atlas"
+    exit
+fi
+
 if [[ $1 == "atlas" ]]; then
     echo "Are you sure you would like to DESTROY and RESET the deployment database? (y/n):"
     read answer
