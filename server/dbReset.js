c5541c9cf44a1ee8455146af3a5616235c9d1d07
Misu Ballo <dizsmek@gmail.com>
2020-06-18 21:06:24 +0200

    db reset functionality almost working... almost

--- server/dbReset.js
+++ server/dbReset.js
@@ -1,3 +1,60 @@
 const log4js = require('log4js');
 const log = log4js.getLogger('generateFinanceAccounts.js');
-const { mongoose } = require('../src/controllers/mongoDB.controller');
+const { mongoose } = require('./src/controllers/mongoDB.controller');
+
+const { Member } = require('./src/models/member.model');
+const { RegisteredUser } = require('./src/models/registered.user.model');
+const { initDBConnection } = require('./src/controllers/mongoDB.controller');
+
+const wipeCollections = async () => {
+    await Member.deleteMany({})
+        .then(() => log.info('Members wiped successfully!'))
+        .catch((error) => {
+            log.error(error);
+            mongoose.disconnect();
+        });
+    await RegisteredUser.deleteMany({})
+        .then(() => log.info('Registered users wiped successfully!'))
+        .catch((error) => {
+            log.error(error);
+            mongoose.disconnect();
+        });
+};
+
+const createMember = (label, email) => {
+    return Member.create({
+        label: label,
+        email: email
+    });
+};
+
+const createMemberPromises = (memberArray) => {
+    try {
+        const promiseArray = [];
+        memberArray.forEach(member => {
+            promiseArray.push(createMember(member.label, member.email));
+        });
+        Promise.all(promiseArray)
+            .then((res) => {
+                res.forEach(member => {
+                    log.info(`Document in 'members' successfully created for ${member.email}`);
+                });
+            })
+            .then(mongoose.disconnect());
+    } catch (error) {
+        log.error(error);
+        mongoose.disconnect();
+    }
+};
+
+const executeWipeAndCreate = async () => {
+    initDBConnection();
+    await wipeCollections();
+    createMemberPromises([{ email: 'asdf@gmail.com', label: 'Belacska' }, { email: 'sadfasdf@gmail.com', label: 'Jolanka' }]);
+};
+
+executeWipeAndCreate()
+    .catch((error) => {
+        log.error(error);
+        mongoose.disconnect();
+    });
