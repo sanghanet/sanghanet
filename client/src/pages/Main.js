04e58d20cfcff61eb9ebc903f429fd002917f2d3
Misu Ballo <dizsmek@gmail.com>
2020-06-27 18:32:49 +0200

    page name handled in Main.js

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Header from '../components/Header/Header';
@@ -17,37 +17,52 @@ import AdminEvent from '../pages/Admins/AdminEvent/AdminEvent';
 import AdminYoga from '../pages/Admins/AdminYoga/AdminYoga';
 import Superuser from '../pages/Admins/Superuser/Superuser';
 
-class Main extends Component {
-    render () {
-        return (
-            <div className='grid-container'>
-                <Header activePage="Yoga" />
-                <Navbar navStyle="sidenav" />
-                <main className="align">
-                    <Switch>
-                        <Route path='/app/dashboard' render={ (props) => <Dashboard {...props} /> } />
-                        <Route path='/app/personal' render={ (props) => <Personal {...props} /> } />
-                        <Route path='/app/yoga' render={ (props) => <Yoga {...props} /> } />
-                        <Route path='/app/finances' render={ (props) => <Finances {...props} /> } />
-                        <Route path='/app/events' render={ (props) => <Events {...props} /> } />
-                        <Route path='/app/questions' render={ (props) => <Questions {...props} /> } />
-                        <Route path='/app/queries' render={ (props) => <Queries {...props} /> } />
+const Main = (props) => {
+    const [pageName, setPageName] = useState('');
 
-                        <Route path='/app/admin/finance' render={ (props) => <AdminFinance {...props} /> } />
-                        <Route path='/app/admin/event' render={ (props) => <AdminEvent {...props} /> } />
-                        <Route path='/app/admin/yoga' render={ (props) => <AdminYoga {...props} /> } />
-                        <Route path='/app/admin/superuser' render={ (props) => <Superuser {...props} /> } />
+    useEffect(() => {
+        const url = window.location.href;
+        let endOfUrl;
 
-                        <Route exact path='/app/'>
-                            <Redirect to='/app/personal' />
-                        </Route>
-                        <Redirect to='/404' />
-                    </Switch>
-                </main>
-                <Footer />
-            </div>
-        );
-    }
+        if (url.includes('/app/admin')) {
+            endOfUrl = url.substring(url.indexOf('admin'));
+            console.log(endOfUrl);
+        } else {
+            endOfUrl = url.substring(url.indexOf('app') + 4);
+            console.log(endOfUrl);
+        }
+
+        setPageName(endOfUrl);
+    }, [window.location.href]);
+
+    return (
+        <div className='grid-container'>
+            <Header activePage={pageName} />
+            <Navbar navStyle="sidenav" />
+            <main className="align">
+                <Switch>
+                    <Route path='/app/dashboard' render={ (props) => <Dashboard {...props} /> } />
+                    <Route path='/app/personal' render={ (props) => <Personal {...props} /> } />
+                    <Route path='/app/yoga' render={ (props) => <Yoga {...props} /> } />
+                    <Route path='/app/finances' render={ (props) => <Finances {...props} /> } />
+                    <Route path='/app/events' render={ (props) => <Events {...props} /> } />
+                    <Route path='/app/questions' render={ (props) => <Questions {...props} /> } />
+                    <Route path='/app/queries' render={ (props) => <Queries {...props} /> } />
+
+                    <Route path='/app/admin/finance' render={ (props) => <AdminFinance {...props} /> } />
+                    <Route path='/app/admin/event' render={ (props) => <AdminEvent {...props} /> } />
+                    <Route path='/app/admin/yoga' render={ (props) => <AdminYoga {...props} /> } />
+                    <Route path='/app/admin/superuser' render={ (props) => <Superuser {...props} /> } />
+
+                    <Route exact path='/app/'>
+                        <Redirect to='/app/personal' />
+                    </Route>
+                    <Redirect to='/404' />
+                </Switch>
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default Main;
