6c4b102888af29366317821759f81fff60210cd2
Ildiko Rigo <ildiko.bcb@gmail.com>
2020-07-23 21:21:25 +0200

    Bootstrap Tooltip added

--- client/src/components/MemberDetails/MemberDetails.js
+++ client/src/components/MemberDetails/MemberDetails.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import GenericDialog from '../Form/GenericDialog/GenericDialog';
 import { ReactComponent as CopyIcon } from './copy.svg';
-
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import './MemberDetails.scss';
 
 const MemberDetails = (props) => {
+    const [dataCopy, setDataCopy] = useState(false);
     const { closeDialog, selectedMemberData: data } = props;
     // TODO: error handling
-    // TODO: notification
+    // TODO: test copy-paste on real mobile device
+    // FIXME: if input value is longer than one linne, copy icon shrinks
     const copyToClipboard = (event) => {
         navigator.clipboard.writeText(event.currentTarget.attributes[0].textContent);
+        setDataCopy(true);
     };
-
+    const resetClipboardCopy = (event) => {
+        setDataCopy(false);
+    };
+    const renderTooltip = (props) => {
+        return (
+            <Tooltip id="button-tooltip" {...props} show="true">
+                {dataCopy ? 'Copied to clipboard' : 'Copy'}
+            </Tooltip>
+        );
+    };
     const userData = (label, value) => {
         return (
             <div className="member-data">
                 <p className="data-label">{label}</p>
                 <div className="value-container">
                     <p className="data-value">{value}</p>
-                    <button data-attribute={value} onClick={copyToClipboard}><CopyIcon /></button>
+                    <OverlayTrigger
+                        placement="right"
+                        overlay={renderTooltip}
+                    >
+                        <button data-attribute={value} onClick={copyToClipboard} onMouseLeave={resetClipboardCopy}><CopyIcon /></button>
+                    </OverlayTrigger>
                 </div>
             </div>
         );
