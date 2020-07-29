a78bead02ba0065a11c6cce36a4a6d6ff3d4df69
Misu Ballo <dizsmek@gmail.com>
2020-07-29 17:49:38 +0200

    blinking text added

--- client/src/pages/PageNotFound/PageNotFound.js
+++ client/src/pages/PageNotFound/PageNotFound.js
@@ -5,7 +5,7 @@ import NotFoundImg from '../../components/icons/not_found.png';
 const PageNotFound = (props) => {
     return (
         <div className='not-found-page'>
-            <h1>Not found</h1>
+            <h1>PAGE NOT FOUND</h1>
             <img src={NotFoundImg} />
         </div>
     );
