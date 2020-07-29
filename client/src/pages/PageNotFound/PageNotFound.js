bd03d98b7c39de6c50ae8eda2468800665404876
Misu Ballo <dizsmek@gmail.com>
2020-07-29 16:38:39 +0200

    404 basic view ready

--- client/src/pages/PageNotFound/PageNotFound.js
+++ client/src/pages/PageNotFound/PageNotFound.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './PageNotFound.scss';
+import NotFoundImg from '../../components/icons/not_found.png';
 
 const PageNotFound = (props) => {
-    return (<h1>Page not found</h1>);
+    return (
+        <div className='not-found-page'>
+            <h1>Not found</h1>
+            <img src={NotFoundImg} />
+        </div>
+    );
 };
 
 export default PageNotFound;
