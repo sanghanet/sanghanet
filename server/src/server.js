07f0a9436cea42dca1dab1a0def210fed4e7e184
Spartan <spartan@enigma.com>
2020-08-21 16:48:34 +0200

    FIX #93, On reload the app crashes [deployed version]

--- server/src/server.js
+++ server/src/server.js
@@ -21,16 +21,25 @@ const financeRouter = require('./routers/financeRouter');
 app.use(express.static('app'));
 app.use('/registration', express.static('app'));
 app.use('/loading', express.static('app'));
+app.use('/images', express.static('app/images'));
+
 app.use('/app/dashboard', express.static('app'));
 app.use('/app/personal', express.static('app'));
-// app.use('/app/members', express.static('app'));
-app.use('/images', express.static('app/images'));
-app.use('/finances', express.static('app'));
-app.use('/queries', express.static('app'));
+app.use('/app/yoga', express.static('app'));
+app.use('/app/finances', express.static('app'));
+app.use('/app/events', express.static('app'));
+app.use('/app/members', express.static('app'));
+app.use('/app/questions', express.static('app'));
+app.use('/app/queries', express.static('app'));
+
 app.use('/admin/finance', express.static('app'));
 app.use('/admin/event', express.static('app'));
 app.use('/admin/yoga', express.static('app'));
 app.use('/admin/superuser', express.static('app'));
+app.use('/app/admin/finance', express.static('app')); //deploymen add /app/ to the URL
+app.use('/app/admin/event', express.static('app'));
+app.use('/app/admin/yoga', express.static('app'));
+app.use('/app/admin/superuser', express.static('app'));
 
 // configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
