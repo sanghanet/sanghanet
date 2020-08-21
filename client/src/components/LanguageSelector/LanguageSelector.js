8df689eaf7e47290dfdbcf1282c0c5a4206e50cb
Benjamin Jozsa <jozsa.benjamin93@gmail.com>
2020-08-21 17:30:16 +0200

    Make footer lang selector smaller

--- client/src/components/LanguageSelector/LanguageSelector.js
+++ client/src/components/LanguageSelector/LanguageSelector.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { UIcontext } from '../contexts/UIcontext/UIcontext';
 import './LanguageSelector.scss';
+import PropTypes from 'prop-types';
 
 const LanguageSelector = (props) => {
     const defaultLang = localStorage.getItem('lang');
@@ -25,11 +26,15 @@ const LanguageSelector = (props) => {
 
     return (
 
-        <div className = "language-selector" onClick = {handleSliderClick}>
+        <div className = {`language-selector ${props.size}`} onClick = {handleSliderClick}>
             <div className = {`lang-slider ${sliderPos}`}></div>
         </div>
 
     );
+};
+
+LanguageSelector.propTypes = {
+    size: PropTypes.string
 };
 
 export default LanguageSelector;
