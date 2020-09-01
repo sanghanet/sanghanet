cf273c7239c36dcbcc1e47742ad8c62f4d921704
Benjamin Jozsa <jozsa.benjamin93@gmail.com>
2020-09-02 00:29:41 +0200

    Clear an unnecessary import

--- client/src/components/LanguageSelector/LanguageSelector.js
+++ client/src/components/LanguageSelector/LanguageSelector.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import { UIcontext } from '../contexts/UIcontext/UIcontext';
 import './LanguageSelector.scss';
 import PropTypes from 'prop-types';
