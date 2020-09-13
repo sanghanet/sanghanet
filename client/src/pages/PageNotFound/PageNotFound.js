6573178e9272716bea09cf585796c3a9e1f35ae4
nalesnyik <naleslg@gmail.com>
2020-09-13 19:38:41 +0200

    Creates go-back-to-previous-page button on 404

--- client/src/pages/PageNotFound/PageNotFound.js
+++ client/src/pages/PageNotFound/PageNotFound.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './PageNotFound.scss';
 import NotFoundImg from '../../components/icons/not_found.png';
+import GoBack from 'react-bootstrap/Button';
+import { ReactComponent as BackIcon } from '../../components/icons/arrow-left.svg';
+
+const goBack = () => window.history.back();
 
 const PageNotFound = (props) => {
     return (
         <div className='not-found-page'>
             <h1>PAGE NOT FOUND</h1>
+            <GoBack variant="primary" onClick={goBack}>
+                <BackIcon />
+                <h3>Go Back</h3>
+            </GoBack>
             <img src={NotFoundImg} alt='page not found' />
         </div>
     );
