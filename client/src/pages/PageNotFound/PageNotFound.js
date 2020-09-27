588906995031e3c027e85c9e1751c6ba24504038
Kristóf <naleslg@gmail.com>
2020-09-27 21:38:38 +0200

    Sets 'go back' button properly

--- client/src/pages/PageNotFound/PageNotFound.js
+++ client/src/pages/PageNotFound/PageNotFound.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './PageNotFound.scss';
 import NotFoundImg from '../../components/icons/not_found.png';
-import GoBack from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import { ReactComponent as BackIcon } from '../../components/icons/arrow-left.svg';
 
-const goBack = () => window.history.back();
+const goBack = () => {
+    window.location.href = 'app/personal';
+};
 
 const PageNotFound = (props) => {
     return (
         <div className='not-found-page'>
-            <h1>PAGE NOT FOUND</h1>
-            <GoBack variant="primary" onClick={goBack}>
-                <BackIcon />
-                <h3>Go Back</h3>
-            </GoBack>
+            <div className='container'>
+                <Button variant="primary" onClick={goBack}>
+                    <BackIcon />
+                    <h6>Go Back</h6>
+                </Button>
+                <h1>PAGE NOT FOUND</h1>
+            </div>
             <img src={NotFoundImg} alt='page not found' />
         </div>
     );
