f974377b9a0325847b2476a96848947582dfbbdd
Miklos Ballo <miklos.ballo@gmail.com>
2020-11-06 18:39:15 +0100

    404 page styling fixed

--- client/src/pages/PageNotFound/PageNotFound.js
+++ client/src/pages/PageNotFound/PageNotFound.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './PageNotFound.scss';
 import NotFoundImg from '../../components/icons/not_found.png';
 import Button from 'react-bootstrap/Button';
 import { ReactComponent as BackIcon } from '../../components/icons/arrow-left.svg';
+import { UIcontext } from '../../components/contexts/UIcontext/UIcontext';
 
 const goBack = () => {
     window.location.href = 'app/';
 };
 
 const PageNotFound = (props) => {
+    const { PAGENOTFOUND, GOBACK } = useContext(UIcontext).dictionary.pageNotFound;
+
     return (
         <div className='not-found-page'>
-            <div className='container'>
-                <Button variant="primary" onClick={goBack}>
+            <div className='text-container'>
+            <h1>{PAGENOTFOUND}</h1>
+                <Button variant="dark" onClick={goBack}>
                     <BackIcon />
-                    <h6>Go Back</h6>
+                    <p>{GOBACK}</p>
                 </Button>
-                <h1>PAGE NOT FOUND</h1>
             </div>
             <img src={NotFoundImg} alt='page not found' />
         </div>
