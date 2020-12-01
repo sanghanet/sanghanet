c512cafc76c62ff0eb3bb86242fa388ec3e3104d
Miklos Ballo <dizsmek@gmail.com>
2020-12-01 18:07:43 +0100

    Removed unnecessary hooks

--- client/src/components/FinanceDashboard/FinanceDashboard.js
+++ client/src/components/FinanceDashboard/FinanceDashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import './FinanceDashboard.scss';
 import PropTypes from 'prop-types';
 import { UIcontext } from '../contexts/UIcontext/UIcontext';
