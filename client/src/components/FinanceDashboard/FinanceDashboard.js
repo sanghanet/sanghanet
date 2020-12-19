8ba6ab483feb67bbba2023d05b7ece5bd4d3f761
Ildiko Rigo <ildiko.bcb@gmail.com>
2020-12-19 18:09:09 +0100

    Finance summary cards styled

--- client/src/components/FinanceDashboard/FinanceDashboard.js
+++ client/src/components/FinanceDashboard/FinanceDashboard.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './FinanceDashboard.scss';
 import PropTypes from 'prop-types';
+import { ReactComponent as Membership } from '../icons/fin_membership.svg';
+import { ReactComponent as Rent } from '../icons/fin_rent.svg';
+import { ReactComponent as Event } from '../icons/fin_event.svg';
+import { ReactComponent as Angel } from '../icons/fin_angel.svg';
 
 class FinanceDashboard extends React.Component {
     state = {
@@ -12,13 +16,43 @@ class FinanceDashboard extends React.Component {
         this.buildFinanceOverview(this.props.balance, this.props.currency);
     }
 
+    getIcon = (key) => {
+        switch (key) {
+            case 'membership': return <Membership />;
+            case 'rent': return <Rent />;
+            case 'event': return <Event />;
+            case 'angel': return <Angel />;
+        }
+    }
+
+    getValueWithSpace = (value) => {
+        const sign = value >= 0 ? '+' : '-';
+        const absValueStr = Math.abs(value).toString();
+        const result = [];
+
+        for (let i = 1; i <= absValueStr.length; i++) { // entry value ex.: '12319217'
+            result.push(absValueStr.charAt(absValueStr.length - i));
+            if (i % 3 === 0 && i !== absValueStr.length) result.push(' ');
+        }
+        // output: ['7', '1', '2', ' ', '9', '1', '3', ' ', '2', '1']
+        return `${sign}${result.reverse().join('')} `;
+    }
+
     buildFinanceOverview = (balance, currency) => {
         try {
             const categories = [];
             for (const [key, value] of Object.entries(balance)) {
                 categories.push(
-                    <div key = {key}>
-                        {`Current balance of ${key} pocket: ${value} ${currency}`}
+                    <div key = {key} className = 'fin-card'>
+                        <div className='fin-card-1st'>
+                            <div><span className = 'capitalize'>{key}</span> balance:</div>
+                            <div>{this.getIcon(key)}</div>
+                        </div>
+                        <div className={`fin-card-2nd ${value >= 0 ? 'green' : 'red'}`}>
+                            <div>{this.getValueWithSpace(value)}</div>
+                            &nbsp;&nbsp;
+                            <div>{currency}</div>
+                        </div>
                     </div>
                 );
             }
@@ -30,7 +64,7 @@ class FinanceDashboard extends React.Component {
 
     render () {
         return (
-            <div className = "overview" >
+            <div className='outer'>
                 {this.state.balances}
             </div>
         );
