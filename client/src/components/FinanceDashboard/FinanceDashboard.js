bcd23a78144654ae7b966e5f9373995bb2dccac1
Miklos Ballo <dizsmek@gmail.com>
2021-01-03 18:23:35 +0100

    Cherrypicked fin icons and added merge conflict fixes

--- client/src/components/FinanceDashboard/FinanceDashboard.js
+++ client/src/components/FinanceDashboard/FinanceDashboard.js
@@ -1,30 +1,62 @@
 import React, { useContext } from 'react';
 import './FinanceDashboard.scss';
 import PropTypes from 'prop-types';
+import { formatMoney } from '../../languages/InternationalizationMethods';
 import { UIcontext } from '../contexts/UIcontext/UIcontext';
+import { ReactComponent as Membership } from '../icons/fin_membership.svg';
+import { ReactComponent as Rent } from '../icons/fin_rent.svg';
+import { ReactComponent as Event } from '../icons/fin_event.svg';
+import { ReactComponent as Angel } from '../icons/fin_angel.svg';
 
 const FinanceDashboard = (props) => {
     const { financeDashboard, financePockets } = useContext(UIcontext).dictionary;
-    const { BEFOREPOCKETNAME, AFTERPOCKETNAME } = financeDashboard;
+    const { BALANCE } = financeDashboard;
+    const lang = localStorage.getItem('lang');
 
     const { balance, currency } = props;
 
+    const getIcon = (key) => {
+        switch (key) {
+            case 'membership': return <Membership />;
+            case 'rent': return <Rent />;
+            case 'event': return <Event />;
+            case 'angel': return <Angel />;
+            default: return null;
+        }
+    };
+
     return (
-        <div className = "overview" >
-            {Object.entries(balance).map(([key, value]) => {
-                return (
-                    <div key = {key}>
-                        {BEFOREPOCKETNAME + financePockets[key.toUpperCase()] + AFTERPOCKETNAME + `${value} ${currency}`}
-                    </div>
-                );
+        <div className='overview' >
+            {/* eslint-disable-next-line array-callback-return */}
+            {Object.entries(balance).map(([pocket, amount, index]) => {
+                try {
+                    const financePocket = financePockets[pocket.toUpperCase()];
+
+                    return (
+                        <div className='outer'>
+                            <div key={index} className='fin-card'>
+                                <div className='fin-card-1st'>
+                                    <div><span className='capitalize'>{financePocket}</span>{BALANCE}</div>
+                                    <div>{getIcon(pocket)}</div>
+                                </div>
+                                <div className={`fin-card-2nd ${amount >= 0 ? 'green' : 'red'}`}>
+                                    <div>{formatMoney(lang, amount, currency)}</div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                } catch (error) {
+                    props.onError(error);
+                }
             })}
         </div>
     );
-}
+};
 
 FinanceDashboard.propTypes = {
     balance: PropTypes.object.isRequired,
-    currency: PropTypes.string.isRequired
+    currency: PropTypes.string.isRequired,
+    onError: PropTypes.func.isRequired
 };
 
 export default FinanceDashboard;
