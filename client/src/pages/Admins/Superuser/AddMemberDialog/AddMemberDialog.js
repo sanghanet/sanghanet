fd80413167a2ccc718235e9f2891b8539dcd0912
Spartan <spartan@enigma.com>
2021-03-20 18:54:29 +0100

    AddMemberDialog as Functional component

--- client/src/pages/Admins/Superuser/AddMemberDialog/AddMemberDialog.js
+++ client/src/pages/Admins/Superuser/AddMemberDialog/AddMemberDialog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import GenericDialog from '../../../../components/Form/GenericDialog/GenericDialog';
 import { UIcontext } from '../../../../components/contexts/UIcontext/UIcontext';
@@ -9,103 +9,89 @@ import { emailValidationRule, nameValidationRule, validationError } from '../../
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-class AddMemberDialog extends Component {
-    static contextType = UIcontext;
+const AddMemberDialog = ({ closeDialog, addMember }) => {
 
-    state = {
-        emailInputValue: '',
-        labelInputValue: '',
-        emailInvalid: true,
-        labelInvalid: true,
-        labelErrorToken: '',
-        emailErrorToken: ''
+    const [emailInputValue, setEmailInputValue] = useState('');
+    const [labelInputValue, setLabelInputValue] = useState('');
+    const [emailInvalid, setEmailInvalid] = useState(true);
+    const [labelInvalid, setLabelInvalid] = useState(true);
+    const [labelErrorToken, setLabelErrorToken] = useState('');
+    const [emailErrorToken, setEmailErrorToken] = useState('');
+
+    const handleEmailChange = (event) => {
+        setEmailInputValue(event.target.value);
+        setEmailErrorToken(validationError(event.target));
+        setEmailInvalid(!!validationError(event.target));
     }
 
-    handleEmailChange = (event) => {
-        this.setState({
-            emailInputValue: event.target.value,
-            emailErrorToken: validationError(event.target),
-            emailInvalid: !!validationError(event.target)
-        });
+    const handleLabelChange = (event) => {
+        setLabelInputValue(event.target.value);
+        setLabelErrorToken(validationError(event.target));
+        setLabelInvalid(!!validationError(event.target));
     }
 
-    handleLabelChange = (event) => {
-        this.setState({
-            labelInputValue: event.target.value,
-            labelErrorToken: validationError(event.target),
-            labelInvalid: !!validationError(event.target)
-        });
-    }
-
-    handleEnter = (event) => {
-        const { labelInvalid, emailInvalid } = this.state;
-
+    const handleEnter = (event) => {
         if (event.key === 'Enter' && !(labelInvalid || emailInvalid)) {
-            this.handleAddMember();
+            handleAddMember();
         }
     }
 
-    handleAddMember = (event) => {
-        const { emailInvalid, labelInvalid } = this.state;
-
+    const handleAddMember = (event) => {
         if (!(emailInvalid && labelInvalid)) {
-            this.props.addMember(`${this.state.emailInputValue}@gmail.com`, this.state.labelInputValue);
+            addMember(`${emailInputValue}@gmail.com`, labelInputValue);
         }
-
         event && event.preventDefault();
     }
 
-    render () {
-        const { closeDialog } = this.props;
-        const { emailErrorToken, labelErrorToken, emailInputValue, labelInputValue, emailInvalid, labelInvalid } = this.state;
-        const { POPUPADDMEMBER, POPUPNAME, POPUPEMAIL } = this.context.dictionary.superuserAddMember;
-        const { CANCEL, ADD } = this.context.dictionary.modalButtons;
-        const { validationMsg } = this.context.dictionary;
+    const {
+        modalButtons: { CANCEL, ADD },
+        superuserAddMember: { POPUPADDMEMBER, POPUPNAME, POPUPEMAIL },
+        validationMsg
+    } = useContext(UIcontext).dictionary;
 
-        return (
-            <GenericDialog
-                title = {POPUPADDMEMBER}
-                reject = {CANCEL}
-                accept = {ADD}
-                acceptDisabled = {emailInvalid || labelInvalid}
-                handleClose = {closeDialog}
-                handleAccept = {this.handleAddMember}
-            >
-                <Form onSubmit={this.handleAddMember} autoComplete='off' className="add-member-dialog">
-                    <Form.Label htmlFor="label-input">{POPUPNAME}<span>*</span></Form.Label>
+    return (
+        <GenericDialog
+            title = {POPUPADDMEMBER}
+            reject = {CANCEL}
+            accept = {ADD}
+            acceptDisabled = {emailInvalid || labelInvalid}
+            handleClose = {closeDialog}
+            handleAccept = {handleAddMember}
+        >
+            <Form onSubmit={handleAddMember} autoComplete='off' className="add-member-dialog">
+                <Form.Label htmlFor="label-input">{POPUPNAME}<span>*</span></Form.Label>
+                <Form.Control
+                    className={labelErrorToken.length ? 'label-input invalid' : 'label-input'}
+                    type="text"
+                    value={labelInputValue}
+                    id="label-input"
+                    onChange={handleLabelChange}
+                    onKeyPress={handleEnter}
+                    autoFocus
+                    {...nameValidationRule}
+                >
+                </Form.Control>
+                <span className="error" aria-live="polite">{validationMsg[labelErrorToken]}</span>
+                <Form.Label htmlFor="email-input">{POPUPEMAIL}<span>*</span></Form.Label>
+                <InputGroup>
                     <Form.Control
-                        className={labelErrorToken.length ? 'label-input invalid' : 'label-input'}
+                        className={emailErrorToken.length ? 'email-input invalid' : 'email-input'}
                         type="text"
-                        value={labelInputValue}
-                        id="label-input"
-                        onChange={this.handleLabelChange}
-                        onKeyPress={this.handleEnter}
-                        autoFocus
-                        {...nameValidationRule}
+                        value={emailInputValue}
+                        id="email-input"
+                        onChange={handleEmailChange}
+                        onKeyPress={handleEnter}
+                        {...emailValidationRule}
                     >
                     </Form.Control>
-                    <span className="error" aria-live="polite">{validationMsg[labelErrorToken]}</span>
-                    <Form.Label htmlFor="email-input">{POPUPEMAIL}<span>*</span></Form.Label>
-                    <InputGroup>
-                        <Form.Control
-                            className={emailErrorToken.length ? 'email-input invalid' : 'email-input'}
-                            type="text"
-                            value={emailInputValue}
-                            id="email-input"
-                            onChange={this.handleEmailChange}
-                            onKeyPress={this.handleEnter}
-                            {...emailValidationRule}
-                        >
-                        </Form.Control>
-                        <InputGroup.Append>
-                            <InputGroup.Text>@gmail.com</InputGroup.Text>
-                        </InputGroup.Append>
-                    </InputGroup>
-                    <span className="error" aria-live="polite">{validationMsg[emailErrorToken]}</span>
-                </Form>
-            </GenericDialog>
-        );
-    }
+                    <InputGroup.Append>
+                        <InputGroup.Text>@gmail.com</InputGroup.Text>
+                    </InputGroup.Append>
+                </InputGroup>
+                <span className="error" aria-live="polite">{validationMsg[emailErrorToken]}</span>
+            </Form>
+        </GenericDialog>
+    );
 }
 
 AddMemberDialog.propTypes = {
