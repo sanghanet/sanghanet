15442a52879183358bb3221f6080330b9d387f4f
Spartan <spartan@enigma.com>
2021-04-04 21:34:14 +0200

    CustomDateInput 2 Typescript

--- client/src/components/Form/CustomDateInput/CustomDateInput.tsx
+++ client/src/components/Form/CustomDateInput/CustomDateInput.tsx
@@ -3,8 +3,14 @@ import PropTypes from 'prop-types';
 import './CustomDateInput.scss';
 // https://www.npmjs.com/package/react-datepicker
 // Source: https://github.com/Hacker0x01/react-datepicker/issues/862
+// For TS: https://github.com/Hacker0x01/react-datepicker/issues/2165
 
-const CustomDateInput = ({ onClick, value }) => {
+interface CustomDateInputProps {
+    onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+    value?: string
+};
+
+const CustomDateInput: React.FC<CustomDateInputProps> = ({ onClick, value }) => {
     return <button type="button" onClick={onClick}>{value}</button>;
 };
 
