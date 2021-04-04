5b6e032ffe6eec069a98628510f1c85191310952
Spartan <spartan@enigma.com>
2021-04-04 21:11:10 +0200

    CustomDateInput as Functional component

--- client/src/components/Form/CustomDateInput/CustomDateInput.js
+++ client/src/components/Form/CustomDateInput/CustomDateInput.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './CustomDateInput.scss';
 // https://www.npmjs.com/package/react-datepicker
 // Source: https://github.com/Hacker0x01/react-datepicker/issues/862
 
-class CustomDateInput extends Component {
-    render () {
-        const { onClick, value } = this.props;
-        return <button type="button" onClick={onClick}>{value}</button>;
-    }
-}
+const CustomDateInput = ({ onClick, value }) => {
+    return <button type="button" onClick={onClick}>{value}</button>;
+};
 
 CustomDateInput.propTypes = {
     onClick: PropTypes.func,
