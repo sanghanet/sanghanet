6ca9b0853c3cbc82ee1acfc99692a31b4314a331
Spartan <spartan@enigma.com>
2021-04-04 22:47:17 +0200

    Navbar as Functional component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
@@ -18,18 +18,19 @@ import { ReactComponent as ForwardIcon } from '../icons/arrow-right.svg';
 import { ReactComponent as SuperuserIcon } from '../icons/superman.svg';
 import { UIcontext } from '../contexts/UIcontext/UIcontext';
 
-class Navbar extends Component {
-    state = {
-        showSubmenu: this.props.openSubmenu
-    }
+const Navbar = ({ openSubmenu, navStyle }) => {
+    const [showSubmenu, setShowSubmenu] = useState(openSubmenu);
 
-    handleSubmenu = (event) => {
+    const context = useContext(UIcontext);
+    const { isFinanceAdmin, isEventAdmin, isYogaAdmin, isSuperuser } = context;
+    const isAdmin = isFinanceAdmin || isEventAdmin || isYogaAdmin || isSuperuser;
+
+    const handleSubmenu = (event) => {
         event.stopPropagation(); // w/o this, bubbling event close the Hamburger in App.js!
-        this.setState((prevState) => ({ showSubmenu: !prevState.showSubmenu }));
+        setShowSubmenu(!showSubmenu);
     }
 
-    handleLink = (event) => {
-        const { isFinanceAdmin, isEventAdmin, isYogaAdmin, isSuperuser } = this.context;
+    const handleLink = (event) => {
         const page = event.target.href.split('/').pop();
         switch (page) {
             case 'finance': !isFinanceAdmin && event.preventDefault(); break;
@@ -40,12 +41,12 @@ class Navbar extends Component {
         }
     }
 
-    handleKeyDown = (event) => {
+    const handleKeyDown = (event) => {
         const keyCode = event.keyCode; // CRSR LEFT(37), CRSR RIGHT(39)
         if (keyCode === 37 || keyCode === 39) event.preventDefault();
     }
 
-    createMainMenuItem = (menuItem, index) => {
+    const createMainMenuItem = (menuItem, index) => {
         return (
             <li key={index}>
                 <NavLink exact to={`/${menuItem.path}`} className="link">
@@ -56,89 +57,81 @@ class Navbar extends Component {
         );
     }
 
-    createSubMenuItem = (menuItem, index) => {
+    const createSubMenuItem = (menuItem, index) => {
         return (
             <li key={index}>
                 <div className="sub-link">
                     <NavLink exact to={`/${menuItem.path}`}
                         className={`sub-title${menuItem.isEnabled ? '' : ' disabled'}`}
-                        onClick={this.handleLink}>{ menuItem.label }
+                        onClick={handleLink}>{ menuItem.label }
                     </NavLink>
                 </div>
             </li>
         );
     }
 
-    render () {
-        const { navStyle } = this.props;
-        const { isFinanceAdmin, isEventAdmin, isYogaAdmin, isSuperuser } = this.context;
-        const isAdmin = isFinanceAdmin || isEventAdmin || isYogaAdmin || isSuperuser;
-        const classList = this.state.showSubmenu ? 'wrapper show-submenu' : 'wrapper';
+    const classList = showSubmenu ? 'wrapper show-submenu' : 'wrapper';
+    const {
+        ADMINS,
+        BACK,
+        DASHBOARD,
+        PERSONAL,
+        YOGA,
+        FINANCES,
+        EVENTS,
+        MEMBERS,
+        QUESTIONS,
+        QUERIES,
+        FINANCE_ADMIN,
+        EVENT_ADMIN,
+        YOGA_ADMIN,
+        SUPERUSER
+    } = context.dictionary.pageAndNavbarTitles;
 
-        const {
-            ADMINS,
-            BACK,
-            DASHBOARD,
-            PERSONAL,
-            YOGA,
-            FINANCES,
-            EVENTS,
-            MEMBERS,
-            QUESTIONS,
-            QUERIES,
-            FINANCE_ADMIN,
-            EVENT_ADMIN,
-            YOGA_ADMIN,
-            SUPERUSER
-        } = this.context.dictionary.pageAndNavbarTitles;
+    const mainMenu = [
+        { path: 'app/dashboard', icon: DashboardIcon, label: DASHBOARD },
+        { path: 'app/personal', icon: PersonalIcon, label: PERSONAL },
+        { path: 'app/yoga', icon: YogaIcon, label: YOGA },
+        { path: 'app/finances', icon: FinancesIcon, label: FINANCES },
+        { path: 'app/events', icon: EventsIcon, label: EVENTS },
+        { path: 'app/members', icon: MembersIcon, label: MEMBERS },
+        { path: 'app/questions', icon: QuestionsIcon, label: QUESTIONS },
+        { path: 'app/queries', icon: QueriesIcon, label: QUERIES }];
 
-        const mainMenu = [
-            { path: 'app/dashboard', icon: DashboardIcon, label: DASHBOARD },
-            { path: 'app/personal', icon: PersonalIcon, label: PERSONAL },
-            { path: 'app/yoga', icon: YogaIcon, label: YOGA },
-            { path: 'app/finances', icon: FinancesIcon, label: FINANCES },
-            { path: 'app/events', icon: EventsIcon, label: EVENTS },
-            { path: 'app/members', icon: MembersIcon, label: MEMBERS },
-            { path: 'app/questions', icon: QuestionsIcon, label: QUESTIONS },
-            { path: 'app/queries', icon: QueriesIcon, label: QUERIES }];
+    const subMenu = [
+        { path: 'app/admin/finance', icon: FinancesIcon, label: FINANCE_ADMIN, isEnabled: isFinanceAdmin },
+        { path: 'app/admin/event', icon: EventsIcon, label: EVENT_ADMIN, isEnabled: isEventAdmin },
+        { path: 'app/admin/yoga', icon: YogaIcon, label: YOGA_ADMIN, isEnabled: isYogaAdmin },
+        { path: 'app/admin/superuser', icon: SuperuserIcon, label: SUPERUSER, isEnabled: isSuperuser }];
 
-        const subMenu = [
-            { path: 'app/admin/finance', icon: FinancesIcon, label: FINANCE_ADMIN, isEnabled: isFinanceAdmin },
-            { path: 'app/admin/event', icon: EventsIcon, label: EVENT_ADMIN, isEnabled: isEventAdmin },
-            { path: 'app/admin/yoga', icon: YogaIcon, label: YOGA_ADMIN, isEnabled: isYogaAdmin },
-            { path: 'app/admin/superuser', icon: SuperuserIcon, label: SUPERUSER, isEnabled: isSuperuser }];
-
-        return (
-            <div id={navStyle}>
-                <div className={classList} onKeyDown={this.handleKeyDown}>
-                    <ul className="main-menu">
-                        <li className="admins">
-                            <div className={`link ${isAdmin || 'disabled'}`} onClick={this.handleSubmenu}>
-                                <div className="menu-icon"><ForwardIcon /></div>
-                                <span className="title admins">{ ADMINS }</span>
-                            </div>
-                        </li>
-                        {mainMenu.map((menuItem, index) => this.createMainMenuItem(menuItem, index))}
-                        <li id="logout-li">
-                            <Logout />
-                        </li>
-                    </ul>
-                    <ul className="sub-menu">
-                        <li className="back">
-                            <div className="link" onClick={this.handleSubmenu}>
-                                <div className="menu-icon"><BackIcon /></div>
-                                <span className="title">{ BACK }</span>
-                            </div>
-                        </li>
-                        {subMenu.map((menuItem, index) => this.createSubMenuItem(menuItem, index))}
-                    </ul>
-                </div>
+    return (
+        <div id={navStyle}>
+            <div className={classList} onKeyDown={handleKeyDown}>
+                <ul className="main-menu">
+                    <li className="admins">
+                        <div className={`link ${isAdmin || 'disabled'}`} onClick={handleSubmenu}>
+                            <div className="menu-icon"><ForwardIcon /></div>
+                            <span className="title admins">{ ADMINS }</span>
+                        </div>
+                    </li>
+                    {mainMenu.map((menuItem, index) => createMainMenuItem(menuItem, index))}
+                    <li id="logout-li">
+                        <Logout />
+                    </li>
+                </ul>
+                <ul className="sub-menu">
+                    <li className="back">
+                        <div className="link" onClick={handleSubmenu}>
+                            <div className="menu-icon"><BackIcon /></div>
+                            <span className="title">{ BACK }</span>
+                        </div>
+                    </li>
+                    {subMenu.map((menuItem, index) => createSubMenuItem(menuItem, index))}
+                </ul>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-Navbar.contextType = UIcontext;
 
 Navbar.propTypes = {
     navStyle: PropTypes.string.isRequired,
