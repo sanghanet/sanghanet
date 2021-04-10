b77df479ddda5afd0d4beb6146be2905b22a63aa
Kristof Nalesnyik <naleslg@gmail.com>
2021-04-10 18:47:16 +0200

    TypeScriptize Transaction Table & Tabs

---
+++ client/src/components/TransactionTable/TransactionTable.tsx
@@ -0,0 +1,129 @@
+import React, { useContext } from 'react';
+import Table from 'react-bootstrap/Table';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import { UIcontext } from '../contexts/UIcontext/UIcontext';
+import { formatMoney, formatDate } from '../../languages/InternationalizationMethods';
+import './TransactionTable.scss';
+import { ReactComponent as Plus } from '../icons/plus.svg';
+import { ReactComponent as Minus } from '../icons/minus.svg';
+import { ReactComponent as Bin } from '../icons/bin.svg';
+import FinanceTransactionProptype from '../../types/FinanceTransactionProptype';
+
+type TransactionTableProps = {
+    transactionArray: Array<FinanceTransactionSchema>;
+    onError: (error: Error) => void; //TODO
+    isFinAdmin: boolean;
+    openAddPayment?: (pocket: string) => void;
+    openAddDebt?: (pocket: string) => void;
+    openDeleteTransaction?: (transaction: TransactionToDelete) => void;
+    pocket: string;
+};
+
+const TransactionTable: React.FC<TransactionTableProps> = (props) => {
+    const {
+        transactionArray,
+        onError,
+        isFinAdmin,
+        openAddPayment,
+        openAddDebt,
+        openDeleteTransaction,
+        pocket,
+    } = props;
+
+    const { ADDNEWPAYMENT, ADDNEWDEBIT, DESCRIPTION, DUEDATE, AMOUNT } = useContext(
+        UIcontext
+    ).dictionary.transactionTable;
+    const lang = localStorage.getItem('lang');
+
+    const onDeleteTransaction: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        const transaction = {
+            id: event.currentTarget.id,
+            pocket: pocket,
+            description: event.currentTarget.dataset.description,
+            amount: parseInt(event.currentTarget.dataset.amount!), //TODO
+            currency: event.currentTarget.dataset.currency,
+            dueDate: event.currentTarget.dataset.duedate,
+        };
+        // @ts-ignore 
+        openDeleteTransaction!(transaction);
+    };
+
+    return (
+        <Table hover bordered variant='dark' className='fn-admin-table'>
+            <thead>
+                {isFinAdmin && (
+                    <tr>
+                        <th colSpan={4} className='trans'>
+                            <Button
+                                className='trans-btn'
+                                variant='success'
+                                onClick={() => openAddPayment!(pocket)}>
+                                <Plus />
+                                {ADDNEWPAYMENT}
+                            </Button>
+                            <Button
+                                className='trans-btn'
+                                variant='danger'
+                                onClick={() => openAddDebt!(pocket)}>
+                                <Minus />
+                                {ADDNEWDEBIT}
+                            </Button>
+                        </th>
+                    </tr>
+                )}
+
+                <tr>
+                    <th>{DESCRIPTION}</th>
+                    <th>{DUEDATE}</th>
+                    <th>{AMOUNT}</th>
+                    {isFinAdmin && <th className='delete-column-header'></th>}
+                </tr>
+            </thead>
+
+            <tbody>
+                {transactionArray.map((transaction, index) => {
+                    const dueDate = formatDate(lang, new Date(transaction.dueDate));
+
+                    return (
+                        <tr className={`finance-row ${transaction.status}`} key={index}>
+                            <td className='description-cell'>{transaction.description}</td>
+                            <td className='date-cell'>{dueDate}</td>
+                            <td className='amount-cell'>{formatMoney(lang, transaction.amount)}</td>
+                            {isFinAdmin && (
+                                <td className='delete-cell'>
+                                    {transaction.status !== 'deleted' && (
+                                        <Button
+                                            variant='outline-danger'
+                                            id={transaction._id}
+                                            onClick={onDeleteTransaction}
+                                            data-description={transaction.description}
+                                            data-amount={transaction.amount}
+                                            data-currency={transaction.currency}
+                                            data-duedate={dueDate}>
+                                            <Bin className='delete-transaction' />
+                                        </Button>
+                                    )}
+                                </td>
+                            )}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </Table>
+    );
+};
+
+TransactionTable.propTypes = {
+    transactionArray: PropTypes.arrayOf(FinanceTransactionProptype).isRequired,
+    onError: PropTypes.func.isRequired,
+    isFinAdmin: PropTypes.bool.isRequired,
+    openAddPayment: PropTypes.func,
+    openAddDebt: PropTypes.func,
+    openDeleteTransaction: PropTypes.func,
+    pocket: PropTypes.string.isRequired,
+};
+
+export default TransactionTable;
---
+++ client/src/components/TransactionTabs/TransactionTabs.tsx
@@ -0,0 +1,65 @@
+import React, { useContext } from 'react';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import TransactionTable from '../TransactionTable/TransactionTable';
+import { UIcontext } from '../contexts/UIcontext/UIcontext';
+import './TransactionTabs.scss';
+import PropTypes from 'prop-types';
+import TransactionsProptype from '../../types/TransactionsProptype';
+
+type TransactionTabsProps = {
+    transactions: Transactions;
+    onError: (error: any) => void; //TODO
+    isFinAdmin: boolean;
+    openAddPayment?: (pocket: string) => void;
+    openAddDebt?: (pocket: string) => void;
+    openDeleteTransaction?: (transaction: TransactionToDelete) => void;
+    activeTab?: string;
+};
+
+const TransactionTabs: React.FC<TransactionTabsProps> = (props) => {
+    const {
+        transactions,
+        onError,
+        isFinAdmin,
+        openAddPayment,
+        openAddDebt,
+        openDeleteTransaction,
+        activeTab,
+    } = props;
+    const { financePockets } = useContext(UIcontext).dictionary;
+
+    return (
+        <Tabs className='MainTabs' bsPrefix='active' defaultActiveKey={activeTab}>
+            {Object.entries(transactions).map((pocket) => {
+                const tabTitle = financePockets[pocket[0].toUpperCase()];
+
+                return (
+                    <Tab title={tabTitle} eventKey={pocket[0]} key={pocket[0]}>
+                        <TransactionTable
+                            transactionArray={pocket[1]}
+                            isFinAdmin={isFinAdmin}
+                            openAddPayment={openAddPayment}
+                            openAddDebt={openAddDebt}
+                            openDeleteTransaction={openDeleteTransaction}
+                            onError={onError}
+                            pocket={pocket[0]}
+                        />
+                    </Tab>
+                );
+            })}
+        </Tabs>
+    );
+};
+
+TransactionTabs.propTypes = {
+    transactions: TransactionsProptype,
+    onError: PropTypes.func.isRequired,
+    isFinAdmin: PropTypes.bool.isRequired,
+    openAddPayment: PropTypes.func,
+    openAddDebt: PropTypes.func,
+    openDeleteTransaction: PropTypes.func,
+    activeTab: PropTypes.string,
+};
+
+export default TransactionTabs;
