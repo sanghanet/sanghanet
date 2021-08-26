333ab08f22eefef46f23f0d554d45b6ef1904e64
Kristof Nalesnyik <naleslg@gmail.com>
2021-08-26 17:12:25 +0200

    Fix login pages indentation

--- test/wdio/test/page-objects/GoogleLogin.page.ts
+++ test/wdio/test/page-objects/GoogleLogin.page.ts
@@ -1,45 +1,44 @@
 class GoogleLogin {
+    get emailInput() {
+        return $('input[type=email]');
+    }
 
-   get emailInput() {
-      return $("input[type=email]");
-   }
+    get nextButton() {
+        return $('#identifierNext > div > button');
+    }
 
-   get nextButton() {
-      return $("#identifierNext > div > button");
-   }
+    get loginButton() {
+        return $('#passwordNext > div > button');
+    }
 
-   get loginButton() {
-      return $("#passwordNext > div > button");
-   }
+    get passwordInput() {
+        return $('input[name=password]');
+    }
 
-   get passwordInput() {
-      return $("input[name=password]");
-   }
+    get hiddenPassword() {
+        return $('[name=hiddenPassword]');
+    }
 
-   get hiddenPassword() {
-      return $("[name=hiddenPassword]");
-   }
+    clickNextButton() {
+        this.nextButton.click();
+    }
+    clickLoginButton() {
+        this.loginButton.click();
+    }
 
-   clickNextButton() {
-      this.nextButton.click();
-   }
-   clickLoginButton() {
-      this.loginButton.click();
-   }
+    enterEmail(emailAddress: string) {
+        this.emailInput.setValue(emailAddress);
+    }
 
-   enterEmail(emailAddress: string) {
-      this.emailInput.setValue(emailAddress);
-   }
+    enterPassword(password: string) {
+        const passwordInput = this.passwordInput;
+        passwordInput.waitForDisplayed({ timeout: 10000 });
+        passwordInput.setValue(password);
+    }
 
-   enterPassword(password: string) {
-      const passwordInput = this.passwordInput;
-      passwordInput.waitForDisplayed({ timeout: 10000 });
-      passwordInput.setValue(password);
-   }
-
-   waitForPasswordFieldLoaded() {
-      this.hiddenPassword.waitForExist({ reverse: true });
-   }
+    waitForPasswordFieldLoaded() {
+        this.hiddenPassword.waitForExist({ reverse: true });
+    }
 }
 
 export default new GoogleLogin();
--- test/wdio/test/page-objects/Login.page.ts
+++ test/wdio/test/page-objects/Login.page.ts
@@ -1,17 +1,16 @@
-import BasePersonalInfo from "./BasePersonalInfo";
+import BasePersonalInfo from './BasePersonalInfo';
 class LoginPage extends BasePersonalInfo {
-   get loginButton() {
-      return $(".login-btn");
-   }
+    get loginButton() {
+        return $('.login-btn');
+    }
 
-   open() {
-      browser.url("http://localhost:3000");
-   }
+    open() {
+        browser.url('http://localhost:3000');
+    }
 
-   clickLoginButton() {
-      this.loginButton.click();
-   }
+    clickLoginButton() {
+        this.loginButton.click();
+    }
 }
 
 export default new LoginPage();
-
