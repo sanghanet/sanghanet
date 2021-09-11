c1397be1f4e207a1107a0da9b9812c8d0b79c436
Kristof Nalesnyik <naleslg@gmail.com>
2021-09-11 17:12:20 +0200

    Add due-date filter and dictionaries

--- client/src/pages/Finances/FinanceContainer/FinanceContainer.tsx
+++ client/src/pages/Finances/FinanceContainer/FinanceContainer.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Client from '../../../components/Client';
 import './FinanceContainer.scss';
 import FinanceDashboard from '../../../components/FinanceDashboard/FinanceDashboard';
 import TransactionTabs from '../../../components/TransactionTabs/TransactionTabs';
 import Alert from '../../../components/Alert/Alert';
 import PropTypes from 'prop-types';
-import { Form } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap';
 import { addMonths } from 'date-fns';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { UIcontext } from '../../../components/contexts/UIcontext/UIcontext';
 
 interface FinanceContainerProps {
     selectedUser?: string;
@@ -20,9 +21,9 @@ interface FinanceContainerProps {
 }
 
 enum DeletedFilter {
-    All = 'All',
-    Active = 'Active',
-    Deleted = 'Deleted',
+    ALL = 'ALL',
+    ACTIVE = 'ACTIVE',
+    DELETED = 'DELETED',
 }
 
 const FinanceContainer: React.FC<FinanceContainerProps> = (props) => {
@@ -38,12 +39,16 @@ const FinanceContainer: React.FC<FinanceContainerProps> = (props) => {
     const [errorState, setErrorState] = useState(0);
     const [reRender, setReRender] = useState(0); // fine HACK to rerender Component when new data is available.
     const [activeTab, setActiveTab] = useState(activeTabFromAdmin);
-    const [deletedTransactionsFilter, setDeletedTransactionsFilter] = useState<DeletedFilter>(isFinAdmin ? DeletedFilter.All : DeletedFilter.Active);
+    const [deletedTransactionsFilter, setDeletedTransactionsFilter] = useState<DeletedFilter>(
+        isFinAdmin ? DeletedFilter.ALL : DeletedFilter.ACTIVE
+    );
     const [dueDateFromFilter, setDueDateFromFilter] = useState<Date | null>(null);
     const [dueDateToFilter, setDueDateToFilter] = useState<Date | null>(null);
     const dateFormat = 'MM/yyyy';
     const minDate = addMonths(new Date(), -18);
     const maxDate = addMonths(new Date(), 6);
+
+    const { deletedTransactionsFilterTypes, transactionFilterLabels } = useContext(UIcontext).dictionary;
 
     const setLastDayOfMonth = (date: Date): Date => {
         const year = date.getFullYear();
@@ -123,24 +128,48 @@ const FinanceContainer: React.FC<FinanceContainerProps> = (props) => {
         setDeletedTransactionsFilter(event.target.value as DeletedFilter);
     };
 
-    const filterTransactionsByDeletedFlag = (financeTransactions: Transactions): Transactions => {
-        const transactions: Transactions = JSON.parse(JSON.stringify(financeTransactions));
+    const transactionsFilterHelper = (transactions: Transactions, predicate: (x: FinanceTransaction) => boolean): Transactions => {
+        for (const key in transactions) {
+            transactions[key as Pocket] = transactions[key as Pocket].filter(predicate);
+        }
 
+        return transactions;
+    };
+
+    const filterTransactionsByDeletedFlag = (transactions: Transactions): Transactions => {
         const filter = (showDeleted: boolean): Transactions => {
-            for (const key in transactions) {
-                transactions[key as Pocket] = transactions[key as Pocket].filter((finTransaction) =>
-                    showDeleted ? finTransaction.deleted : !finTransaction.deleted
-                );
-            }
-
-            return transactions;
+            const predicate = (t: FinanceTransaction): boolean => showDeleted ? !!t.deleted : !t.deleted;
+            return transactionsFilterHelper(transactions, predicate);
         };
 
         switch (deletedTransactionsFilter) {
-            case DeletedFilter.Active: return filter(false);
-            case DeletedFilter.Deleted: return filter(true);
-            default: return financeTransactions;
+            case DeletedFilter.ACTIVE:
+                return filter(false);
+            case DeletedFilter.DELETED:
+                return filter(true);
+            default:
+                return transactions;
         }
+    };
+
+    const filterTransactionsByDueDate = (transactions: Transactions): Transactions => {
+        if (dueDateFromFilter != null) {
+            const predicate = (t: FinanceTransaction): boolean => new Date(t.dueDate) >= dueDateFromFilter;
+            transactions = transactionsFilterHelper(transactions, predicate);
+        }
+
+        if (dueDateToFilter != null) {
+            const predicate = (t: FinanceTransaction): boolean => dueDateToFilter >= new Date(t.dueDate);
+            transactions = transactionsFilterHelper(transactions, predicate);
+        }
+        return transactions;
+    };
+
+    const filterTransactions = (financeTransactions: Transactions): Transactions => {
+        let transactions: Transactions = JSON.parse(JSON.stringify(financeTransactions));
+        transactions = filterTransactionsByDeletedFlag(transactions);
+        transactions = filterTransactionsByDueDate(transactions);
+        return transactions;
     };
 
     return (
@@ -164,21 +193,20 @@ const FinanceContainer: React.FC<FinanceContainerProps> = (props) => {
                     />
                     {isFinAdmin && (
                         <Form className="transactions-filter-form">
-                            <Form.Group className="deleted-filter">
-                                <Form.Label>Show deleted</Form.Label>
+                            <Form.Group controlId="deleted" className="deleted-filter">
+                                <Form.Label>{transactionFilterLabels.SHOW_DELETED}</Form.Label>
                                 <Form.Control
-                                    id="deletedFilter"
                                     onChange={handleChange}
                                     defaultValue={deletedTransactionsFilter}
                                     as="select"
                                 >
-                                    <option value={DeletedFilter.All}>All</option>
-                                    <option value={DeletedFilter.Active}>Active</option>
-                                    <option value={DeletedFilter.Deleted}>Deleted</option>
+                                    <option value={DeletedFilter.ALL}>{deletedTransactionsFilterTypes[DeletedFilter.ALL]}</option>
+                                    <option value={DeletedFilter.ACTIVE}>{deletedTransactionsFilterTypes[DeletedFilter.ACTIVE]}</option>
+                                    <option value={DeletedFilter.DELETED}>{deletedTransactionsFilterTypes[DeletedFilter.DELETED]}</option>
                                 </Form.Control>
                             </Form.Group>
                             <Form.Group className="date-picker">
-                                <Form.Label>From start of</Form.Label>
+                                <Form.Label>{transactionFilterLabels.DUE_DATE_FROM}</Form.Label>
                                 <DatePicker
                                     id="due-date-from"
                                     className="form-control"
@@ -192,7 +220,7 @@ const FinanceContainer: React.FC<FinanceContainerProps> = (props) => {
                                     showDisabledMonthNavigation
                                     // isClearable
                                 />
-                                <Form.Label>To end of</Form.Label>
+                                <Form.Label>{transactionFilterLabels.DUE_DATE_TO}</Form.Label>
                                 <DatePicker
                                     id="due-date-to"
                                     className="form-control"
@@ -210,7 +238,7 @@ const FinanceContainer: React.FC<FinanceContainerProps> = (props) => {
                         </Form>
                     )}
                     <TransactionTabs
-                        transactions={filterTransactionsByDeletedFlag(financeData.transactions)}
+                        transactions={filterTransactions(financeData.transactions)}
                         onError={handleError}
                         isFinAdmin={isFinAdmin}
                         openAddPayment={openAddPayment}
